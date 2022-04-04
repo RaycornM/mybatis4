79bde40c434c9141e63b569fb0e94d988ebd2fba
raycorn <1228082879@qq.com>
2022-04-04 16:23:10 +0800

    first commit

---
+++ src/main/java/com/tedu/mapper/EmpMapper.java
@@ -0,0 +1,14 @@
+package com.tedu.mapper;
+
+import java.util.List;
+
+import com.tedu.bean.EmpBean;
+
+//dao mapper的接口类
+public interface EmpMapper {
+	//查询所有数据
+	public List<EmpBean> selectAllEmp();
+	//查询单个数据，依据一个唯一id
+	public EmpBean selectEmpById(int id);
+	
+}
---
+++ src/main/java/com/tedu/test/TestMapper.java
@@ -0,0 +1,33 @@
+package com.tedu.test;
+
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.mysql.jdbc.Connection;
+import com.tedu.bean.EmpBean;
+import com.tedu.mapper.EmpMapper;
+
+public class TestMapper {
+	//在开发中，编写的测试代码的方法名建议和需要测试的方法名称一致
+	@Test
+	public void selectEmpById() {
+		InputStream in = TestMapper.class.getClassLoader().getResourceAsStream("mybatis-configs.xml");
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+		//这个返回值sqlSession可以认为是Connection statement
+		SqlSession session = sessionFactory.openSession();
+		System.out.println(session);
+		//得到这个接口类型的实体对象
+		EmpMapper mapper = session.getMapper(EmpMapper.class);
+		//在select查询中，会直接当字段名称和bean的属性名称一一做映射
+		EmpBean bean = mapper.selectEmpById(4);
+		System.out.println(bean);
+	}
+}
